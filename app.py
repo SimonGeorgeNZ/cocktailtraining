2fcf557f4fdef8aa1307a0fe6aab1676310000a5
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2021-08-23 04:15:30 +0000

    linked with MongoDB and displaying data

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+import os
+from flask import Flask, render_template, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
-from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'CocktailTraining'
+app.config["MONGO_URI"] = 'mongodb+srv://root:Dunedin100@myfirstcluster.jekwe.mongodb.net/CocktailTraining?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def home():
-    return "Hello! This is my main page <h1>HELLO</h1>"
+    test = mongo.db.ingredients.find()
+    return render_template('index.html', test=test)
+
 
 if __name__ == "__main__":
     app.run()
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
