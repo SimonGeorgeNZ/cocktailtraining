bf0d6dcdc508b6daafaf6a52e57a39af52af1658
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2021-10-15 16:53:07 +0000

    multi and YN filtering properly

--- app.py
+++ app.py
@@ -4,14 +4,10 @@ from flask import Flask, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-from class_ import *
 
 app = Flask(__name__)
-
-class MongoInfo():
-
-    app.config["MONGO_DBNAME"] = 'CocktailTraining'
-    app.config["MONGO_URI"] = 'mongodb+srv://root:Dunedin100@myfirstcluster.jekwe.mongodb.net/CocktailTraining?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'CocktailTraining'
+app.config["MONGO_URI"] = 'mongodb+srv://root:Dunedin100@myfirstcluster.jekwe.mongodb.net/CocktailTraining?retryWrites=true&w=majority'
 
 
 mongo = PyMongo(app) 
@@ -19,14 +15,37 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def home():
+    class InitialInfo:
+        YN = ['strained', 'double strained']
+        multi = ['garnish', 'glass', 'base spirit', 'ingredients', 'steps']  #List of entry for question
+        sub = ['YN', 'multi']
+        SubChoice = random.choice(sub)  # Finds random question
+        if SubChoice == 'multi':
+            pickSub = random.choice(multi)
+        else:
+            pickSub = random.choice(YN)
+        cocktailsAll = mongo.db.cocktail.find()
+        allNames = cocktailsAll.distinct("name")
 
-    pickSB = initialInfo().pickSub
+    class Question:
+        CTnames = InitialInfo.allNames
+        NameList = [CTnames]
+        for names in NameList:
+            choices = random.sample(names, 4)
+            correct = random.choice(choices)
+            FullCT = mongo.db.cocktail.find_one({"name" : correct})
+            AnsName = correct
+            choices = choices
+
+
+    sc = InitialInfo.SubChoice
+    pickSB = InitialInfo.pickSub
     fullCT = Question.FullCT
     ansName = Question.AnsName
     choices = Question.choices
 
     return render_template('index.html', 
-                            ps=pickSB, full=fullCT, name=ansName, choices=choices)
+                            ps=pickSB, full=fullCT, name=ansName, choices=choices, x=sc)
 
 
 if __name__ == '__main__':
